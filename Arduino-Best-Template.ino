07d293e8dea5b8e2eb09a16fa55c06b503ea3203
Jim Burnham <jim.the.steam.clown@gmail.com>
2025-06-28 13:10:01 -0700

    Update Arduino-Best-Template.ino

--- Arduino-Best-Template.ino
+++ Arduino-Best-Template.ino
@@ -12,9 +12,46 @@ Best Arduino Template <-- Delete This, but keep and fill out the stuff below
 // Design Name:			blink_STEAMClown
 // Description:     adaptation of blink on http://arduino.cc/ <-- Give Credit, If Due
 // Dependencies: 
-// Revision: 
-// Revision 0.02 - Updated for SVCTE Mechatronics Class - 06/27/2025
-// Revision 0.01 - Created 06/27/2025
+// Vesrion / Revision:
+// #define code_version 0.01 //  0.01 - Created 06/27/2025
+// #define code_version 0.02 //  0.02 - Updated for SVCTE Mechatronics Class - 06/27/2025
+#define code_version 1.01 // Updated with additional template items, like for SVCTE Mechatronics Class - 06/28/2025
+#define code_version_date "06/28/2025"
 // Additional Comments: 
 //
 // ============================================================================
+
+// ============================================================================
+void setup() {
+// ------------------------------------------
+  /* Define stuff that needs to happen imidiatly at power on:
+  
+  */
+// ------------------------------------------
+  /* Initialze the Serial Consol I/O
+      Here you can tell the user any saftey tips, requirments, or expectations
+  */  
+  Serial.begin(115200);  // be sure serial monitor is set for 'newline' line ending??
+  Serial.println(" ------------------------------------------ "); 
+  Serial.println(" --- Begin Of SETUP --- "); 
+  Serial.println(" ------------------------------------------ ");
+  Serial.println();
+  Serial.println("Dark Lord Robotics:"); 
+  Serial.println("One Bot to Rule Them All, and in the Darkness, Bind Them..."); 
+  Serial.println();
+  Serial.println(code_version);
+  Serial.println(code_version_date);
+  Serial.println(" ------------------------------------------ "); 
+  Serial.println(" --- Done With SETUP --- "); 
+  Serial.println(" ------------------------------------------ "); 
+  }
+  delay(2000);
+
+}  // end void setup
+
+// ============================================================================
+void loop() {
+// ------------------------------------------
+
+}
+
