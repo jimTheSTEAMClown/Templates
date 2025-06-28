5c8a441ced27462a4a40bc9f27a6f3bf8eb77918
Jim Burnham <jim.the.steam.clown@gmail.com>
2025-06-27 18:46:41 -0700

    Create Arduino-Best-Template.ino

---
+++ Arduino-Best-Template.ino
@@ -0,0 +1,20 @@
+// ------------------------------------------
+Best Arduino Template <-- Delete This, but keep and fill out the stuff below
+// ------------------------------------------
+// =========================================42===================================80
+// Source: STEAM Clown - www.steamclown.org <-- Who Are You
+// GitHub: https://github.com/jimTheSTEAMClown/RepositoryName <-- Link To Repository
+// Hacker: Jim Burnham - STEAM Clown, Engineer, Maker, Propmaster & Adrenologist <-- Author
+// This example code is covered under the GNU Lesser General Public License v3.0 <-- License Type = GPL
+// and any docs and lesson examples is licensed under the CC BY-NC-SA 3.0. <-- CC
+// https://creativecommons.org/licenses/by-nc-sa/3.0/ <-- License Type = CC
+// Create Date:			06/27/2025 
+// Design Name:			blink_STEAMClown
+// Description:     adaptation of blink on http://arduino.cc/ <-- Give Credit, If Due
+// Dependencies: 
+// Revision: 
+// Revision 0.02 - Updated for SVCTE Mechatronics Class - 06/27/2025
+// Revision 0.01 - Created 06/27/2025
+// Additional Comments: 
+//
+// ============================================================================
